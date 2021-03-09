6b9180b549cb460bf60f9bb7befc78867689fa8b
samueltarus <cheboitarus@gmail.com>
2021-03-09 17:55:35 +0300

    first commit

---
+++ src/main/java/com/example/CompSales/CompSalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CompSales/CompSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
